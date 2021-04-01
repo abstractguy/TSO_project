18d6a4a991c10fdce7a3405069b0795bc8e3036d
Samuel Duclos <nomfullcreatif@gmail.com>
2021-04-01 12:13:01 -0400

    Reorganizing stuff.

--- Logiciels/jetson-containers/utils/network/vnc/client.sh
+++ Logiciels/jetson-containers/utils/network/vnc/client.sh
@@ -1,20 +1,33 @@
-sudo apt install tigervnc-viewer
-xtigervncviewer -SecurityTypes VncAuth -passwd /home/sam/.vnc/passwd :1
-sudo xtigervncviewer -SecurityTypes VncAuth -passwd /home/sam/.vnc/passwd :1
-sudo xtigervncviewer -SecurityTypes VncAuth -passwd /home/sam/.vnc/passwd :2
-gsettings set org.gnome.Vino require-encryption false
-sudo xtigervncviewer -SecurityTypes VncAuth -passwd /home/sam/.vnc/passwd :2
-dbus-launch gsettings set org.gnome.Vino authentication-methods "['vnc']"
-dbus-launch gsettings set org.gnome.Vino vnc-password $(echo -n "12345sam12345"|base64)
-sudo xtigervncviewer -SecurityTypes VncAuth -passwd /home/sam/.vnc/passwd :2
-dbus-launch gsettings set org.gnome.Vino alternative-port 5910
-dbus-launch gsettings set org.gnome.Vino use-alternative-port true
+# Note: Just a reorganized, untested print of my history.
+#       Try line by line. Don't mind redundant or missing stuff until tested.
+
+cd ~
 sudo apt update
 sudo apt install gvncviewer tigervnc-viewer remmina
-rm -fr ~/.vnc
+
+gsettings set org.gnome.Vino require-encryption false
+dbus-launch gsettings set org.gnome.Vino authentication-methods "['vnc']"
+dbus-launch gsettings set org.gnome.Vino vnc-password $(echo -n "12345sam12345"|base64)
+dbus-launch gsettings set org.gnome.Vino alternative-port 5910
+dbus-launch gsettings set org.gnome.Vino use-alternative-port true
+
+rm -rf ~/.vnc
 mkdir -p ~/.vnc
 ln -s /etc/ssl/certs/ca-certificates.crt ~/.vnc/x509_ca.pem
+
 mkdir -p ~/.pki/CA
 ln -s /etc/ssl/certs/ca-certificates.crt ~/.pki/CA/cacert.pem
+
+ifconfig
+
 xtigervncviewer -SecurityTypes VncAuth,TLSVnc -passwd /home/sam/.vnc/passwd 192.168.55.1:1
+xtigervncviewer -SecurityTypes VncAuth,TLSVnc -passwd /home/sam/.vnc/passwd 192.168.55.1:2
+
+vncviewer 192.168.55.1:2
 vncviewer 192.168.55.1:1
+
+gvncviewer 192.168.55.1 --display=:1
+sudo gvncviewer 192.168.55.1 --display=:1
+
+remmina
+
--- Logiciels/jetson-containers/utils/network/vnc/server.sh
+++ Logiciels/jetson-containers/utils/network/vnc/server.sh
@@ -1,20 +1,53 @@
+# Note: Just a reorganized, untested print of my history.
+#       Try line by line. Don't mind redundant or missing stuff until tested.
+
+# DISABLE DESKTOP EFFECTS!!!
+
+cd ~
 sudo apt update
 sudo apt install tigervnc-standalone-server tigervnc-viewer
+#vncserver :1
+#tigervncserver -xstartup /usr/bin/xterm
+#sudo tigervncserver -xstartup /usr/bin/xterm
+#x0vncserver -display :0
+
+sudo echo "[Desktop Entry]" > ~/.config/autostart/vino-server.desktop
+sudo echo "Type=Application" >> ~/.config/autostart/vino-server.desktop
+sudo echo "Name=Vino VNC server" >> ~/.config/autostart/vino-server.desktop
+sudo echo "Exec=/usr/lib/vino/vino-server" >> ~/.config/autostart/vino-server.desktop
+sudo echo "NoDisplay=true" >> ~/.config/autostart/vino-server.desktop
+
 dbus-launch gsettings set org.gnome.Vino authentication-methods "['vnc']"
 dbus-launch gsettings set org.gnome.Vino vnc-password $(echo -n "12345sam12345"|base64)
 dbus-launch gsettings set org.gnome.Vino alternative-port 5910
 dbus-launch gsettings set org.gnome.Vino use-alternative-port true
+
+cd /etc/systemd/system/
+sudo bash -c 'echo "[Unit]" > vinostartup.service'
+sudo bash -c 'echo "Description = description about the service" >> vinostartup.service'
+sudo bash -c 'echo "After = network.target" >> vinostartup.service'
+sudo bash -c 'echo "[Service]" >> vinostartup.service'
+sudo bash -c 'echo "ExecStart = /usr/lib/vino/vino-server" >> vinostartup.service'
+sudo bash -c 'echo "[Install]" >> vinostartup.service'
+sudo bash -c 'echo "WantedBy = multi-user.target" >> vinostartup.service'
 sudo systemctl enable vinostartup.service
+sudo systemctl start vinostartup.service
+
 cd /usr/lib/systemd/user/graphical-session.target.wants/
 sudo ln -s ../vino-server.service ./.
 gsettings set org.gnome.Vino prompt-enabled false
 gsettings set org.gnome.Vino require-encryption false
 gsettings set org.gnome.Vino authentication-methods "['vnc']"
 gsettings set org.gnome.Vino vnc-password $(echo -n '12345sam12345' | base64)
-# DISABLE DESKTOP EFFECTS!!!
 sudo reboot
-vncserver -interface 192.168.55.1
-xtigervncviewer -SecurityTypes VncAuth -passwd /home/sam/.vnc/passwd :1
+
+#vncserver -interface 192.168.55.1
+#xtigervncviewer -SecurityTypes VncAuth -passwd /home/sam/.vnc/passwd :1
+vncserver -kill :3
+vncserver -kill :2
 vncserver -kill :1
+export DISPLAY=:1
+xhost +
+sudo xhost +
 vncserver -localhost no -geometry 800x600 -depth 24
 
