03e9bf68020bc62a59f951201b72d027faf3758f
Samuel Duclos <nomfullcreatif@gmail.com>
2021-04-26 11:46:11 -0400

    Wrote script to flash GCODE-compatible protocol firmware.

---
+++ software/arduino-1.8.13/flash.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# File:        software/arduino-1.8.13/flash.sh
+# By:          Samuel Duclos
+# For:         Myself
+# Description: AVRDUDE upload command.
+# Usage:       sudo bash flash.sh
+
+/home/samuel/school/Projets/Final/TSO_project/software/arduino-1.8.13/hardware/tools/avr/bin/avrdude -C/home/samuel/school/Projets/Final/TSO_project/software/arduino-1.8.13/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyUSB0 -b115200 -D -Uflash:w:/home/samuel/school/Projets/Final/TSO_project/software/arduino-1.8.13/firmware.hex:i
