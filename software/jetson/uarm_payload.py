9104e4fe975919efb1050827928f4965c77f9b56
Samuel Duclos <wiseakasha@hotmail.com>
2021-04-27 11:21:19 -0400

    Add files via upload

--- software/jetson/uarm_payload.py
+++ software/jetson/uarm_payload.py
@@ -12,37 +12,47 @@ from utils import uarm_payload
 import argparse
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Test uARM for object detection using I2C VL6180X Time-of-Flight sensor to scan until object is found.', 
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Test uARM for object detection using a camera to scan until object is found.', 
                                      formatter_class=argparse.RawTextHelpFormatter)
-
     parser = add_payload_args(parser)
-    return parser
+    args = parser.parse_known_args()[0]
+    print(vars(args))
+    return args
 
 def main():
+    """Main function."""
     parser = parse_args()
-    args = parser.parse_args()
-    print(vars(args))
 
-    pay = uarm_payload.Payload(initial_x_position=args.first_x_position, 
-                               initial_y_position=args.first_y_position, 
-                               initial_z_position=args.first_z_position, 
-                               vehicle_x_position=args.second_x_position, 
-                               vehicle_y_position=args.second_y_position, 
-                               vehicle_z_position=args.second_z_position, 
-                               balance_x_position=args.third_x_position, 
-                               balance_y_position=args.third_y_position, 
-                               balance_z_position=args.third_z_position, 
-                               uarm_speed=args.speed, 
-                               uarm_tty_port=args.uarm_tty_port, 
-                               uart_delay=args.uart_delay, 
-                               servo_attach_delay=args.servo_attach_delay, 
-                               set_position_delay=args.set_position_delay, 
-                               servo_detach_delay=args.servo_detach_delay, 
-                               pump_delay=args.pump_delay, 
-                               transition_delay=args.transition_delay)
+    try:
+        pay = uarm_payload.Payload(initial_x_position=args.first_x_position, 
+                                   initial_y_position=args.first_y_position, 
+                                   initial_z_position=args.first_z_position, 
+                                   second_x_position=args.second_x_position, 
+                                   second_y_position=args.second_y_position, 
+                                   second_z_position=args.second_z_position, 
+                                   third_x_position=args.third_x_position, 
+                                   third_y_position=args.third_y_position, 
+                                   third_z_position=args.third_z_position, 
+                                   uarm_speed=args.speed, 
+                                   uart_delay=args.uart_delay, 
+                                   servo_attach_delay=args.servo_attach_delay, 
+                                   set_position_delay=args.set_position_delay, 
+                                   servo_detach_delay=args.servo_detach_delay, 
+                                   pump_delay=args.pump_delay, 
+                                   transition_delay=args.transition_delay)
 
-    pay.payload()
+        pay.payload()
+
+    except Exception as e:
+        print(e)
+
+    except KeyboardInterrupt:
+        print('Program was interrupted by user: exiting.')
+
+    finally:
+        # Release resources.
+        print('Done.')
 
 if __name__ == '__main__':
     main()
-
