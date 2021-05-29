2c62bdbba02cffe01535ff0a5497dd2794f48245
Samuel Duclos <nomfullcreatif@gmail.com>
2021-05-28 20:20:43 -0400

    Installing JetPack-4.4.

--- software/jetson/install/install_jetson_nano_sd_card.sh
+++ software/jetson/install/install_jetson_nano_sd_card.sh
@@ -6,7 +6,7 @@
 # Usage:       cd ~/workspace/software/jetson && bash install/install_jetson_nano_sd_card.sh <JETSON_USER>
 # Description: Install JetPack on the Micro-SD card.
 
-JETSON_USER=(echo "${1:-sam}") && \
+JETSON_USER=$(echo "${1:-sam}") && \
 read -s -p "Enter password: " LAPTOP_PASSWORD && \
 echo $LAPTOP_PASSWORD | sudo -S apt-get update && \
 echo $LAPTOP_PASSWORD | sudo -S apt-get -y install screen && \
@@ -17,8 +17,8 @@ echo $LAPTOP_PASSWORD | sudo -S fdisk -l | grep '^Disk /dev/' | cut -d' ' -f2- |
 read -s -p "Make sure to insert your Micro-SD card then press [enter]." DUMMY && \
 echo $LAPTOP_PASSWORD | sudo -S fdisk -l | grep '^Disk /dev/' | cut -d' ' -f2- | cut -d':' -f1 > ./.fdisk_temp_2.txt && \
 SD_CARD=$(echo $LAPTOP_PASSWORD | sudo -S diff ./.fdisk_temp_1.txt ./.fdisk_temp_2.txt | grep "^>" | cut -d' ' -f2-) && \
-echo $LAPTOP_PASSWORD | sudo -S rm ./.fdisk_temp_1.txt ./.fdisk_temp_2.txt && \
 echo "\nMicro-SD card is: ${SD_CARD}" && \
-echo $LAPTOP_PASSWORD | sudo -S dd if=sd-blob-b01.img of=${SD_CARD} bs=1M oflag=direct status=progress && \
-echo $LAPTOP_PASSWORD | sudo -S screen /dev/ttyACM0
+echo $LAPTOP_PASSWORD | sudo -S dd if=sd-blob-b01.img of=${SD_CARD} bs=1M oflag=direct status=progress
+echo $LAPTOP_PASSWORD | sudo -S rm ./.fdisk_temp_1.txt ./.fdisk_temp_2.txt
+#echo $LAPTOP_PASSWORD | sudo -S screen /dev/ttyACM0
 
