abf940d34564ed87e05b0a7708fc53a4d765adc0
Samuel Duclos <nomfullcreatif@gmail.com>
2021-05-28 21:18:35 -0400

    Scripted SD card flashing. Reverting to JetPack-4.4.

--- software/jetson/install/install_jetson_nano_sd_card.sh
+++ software/jetson/install/install_jetson_nano_sd_card.sh
@@ -18,7 +18,8 @@ read -s -p "Make sure to insert your Micro-SD card then press [enter]." DUMMY &&
 echo $LAPTOP_PASSWORD | sudo -S fdisk -l | grep '^Disk /dev/' | cut -d' ' -f2- | cut -d':' -f1 > ./.fdisk_temp_2.txt && \
 SD_CARD=$(echo $LAPTOP_PASSWORD | sudo -S diff ./.fdisk_temp_1.txt ./.fdisk_temp_2.txt | grep "^>" | cut -d' ' -f2-) && \
 echo "\nMicro-SD card is: ${SD_CARD}" && \
-echo $LAPTOP_PASSWORD | sudo -S dd if=sd-blob-b01.img of=${SD_CARD} bs=1M oflag=direct status=progress
+echo $LAPTOP_PASSWORD | sudo -S dd if=sd-blob-b01.img of=${SD_CARD} bs=1M oflag=direct status=progress && \
+rm -rf sd-blob-b01.img jetson-nano-developer-kit-sd-card-image.zip
 echo $LAPTOP_PASSWORD | sudo -S rm ./.fdisk_temp_1.txt ./.fdisk_temp_2.txt
 sync
 #echo $LAPTOP_PASSWORD | sudo -S screen /dev/ttyACM0
